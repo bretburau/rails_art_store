6260c1a3f636e83d01137ec74b446ca4e2481952
bretburau <github email address>
2018-03-16 07:32:14 -0500

    built user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render new_user_path
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
