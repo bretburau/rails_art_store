810e18a2a1d543fc1d1c34d16300c89f945ab0d4
bretburau <github email address>
2018-03-16 06:59:15 -0500

    set up user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates(:password, { :length => { :in => 6..20 } })
+    
   has_secure_password
-  
 end
