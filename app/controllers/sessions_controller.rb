4f8f8e81e3b0d26f63d482bfdb309b6145e438b8
bretburau <github email address>
2018-03-16 13:10:28 -0500

    login system works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to user_path(current_user) if logged_in?
   end
 
   def create
-    raise params.inspect
     @user = User.find_by(email: params[:user][:email])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    raise session.inspect
     redirect_to user_path(@user)
   end
 
   def destroy
-
+    session.delete :email
+    redirect_to login_path
   end
 end
