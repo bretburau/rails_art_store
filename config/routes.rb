b1d466c570ec5d14d909a8edf20bfdd43f4f8dcb
bretburau <github email address>
2018-03-16 13:01:17 -0500

    login form built

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
