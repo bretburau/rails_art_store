b786faeb07ea51cdc59d5f9a00335ec3c433ce47
bretburau <github email address>
2018-03-16 07:35:57 -0500

    User creates and authenticates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates(:password, { :length => { :in => 6..20 } })
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
   has_secure_password
 end
