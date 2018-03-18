59d99129bea3597ee354a2314b28c989057495ac
bretburau <github email address>
2018-03-18 14:37:24 -0500

    added email to user.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
       user.password = SecureRandom.hex
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+      user.email = auth.info.email
       user.save!
     end
   end
