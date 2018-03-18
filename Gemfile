d82ad59fb8ab9e7bfaea8f04c6c03b9012da7324
bretburau <github email address>
2018-03-18 15:52:43 -0500

    implimented dotenv

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'pry'
   gem 'omniauth'
   gem "omniauth-google-oauth2", "~> 0.2.1"
+  gem 'dotenv-rails'
 end
 
 group :development do
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '976713067370-8bj39b1b740a027qli2nm0773td5iha6.apps.googleusercontent.com', 'CBc2sqVOTz2Ky66ygH4JYKN7', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
