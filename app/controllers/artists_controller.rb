269ac50f29178c1e46d37dfb300b34c7740b667c
bretburau <github email address>
2018-03-22 06:05:15 -0500

    building out Artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,21 @@
 class ArtistsController < ApplicationController
+  before_action :get_artist, only: [:show, :index]
+  before_action :require_login, only: [:cpanel]
+
   def show
-    @artist = Artist.find(params[:id])
   end
 
+  def cpanel
+    @artist = current_user
+  end
+
+  def index
+  end
+
+  private
+
+  def get_artist
+    # @artist = Artist.find(params[:id])
+  end
   
 end
