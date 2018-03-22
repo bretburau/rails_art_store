220dd97c57f3345d5dc5d084bfda428e0a8aaa3c
bretburau <github email address>
2018-03-22 06:27:42 -0500

    abstracted artist_pieces

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
   before_action :get_artist, only: [:show, :index]
   before_action :require_login, only: [:cpanel]
+  ##TODO Authorize for Artists only?
 
   def show
   end
