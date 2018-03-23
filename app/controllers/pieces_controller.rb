88d88a2ab3a05d3ca5f16f56cfddea767afca7c1
bretburau <github email address>
2018-03-23 06:57:58 -0500

    Added Piece index page w/nested route

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,14 @@
 class PiecesController < ApplicationController
 
+  def index
+    if params[:artist_id]
+      @pieces = Artist.find(params[:artist_id]).pieces
+    else
+      @pieces = Piece.all
+    end
+  end
+
   def show
-    
+    @piece = Piece.find(params[:id])
   end
 end
