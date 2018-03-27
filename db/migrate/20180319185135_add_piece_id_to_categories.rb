72d2cf83bb4ef1ad3e8c6e74b6aea3eb975e428b
bretburau <github email address>
2018-03-27 11:35:46 -0500

    Added categories to piece show page

--- db/migrate/20180319185135_add_piece_id_to_categories.rb
+++ db/migrate/20180319185135_add_piece_id_to_categories.rb
@@ -1,5 +1,5 @@
 class AddPieceIdToCategories < ActiveRecord::Migration[5.1]
   def change
-    add_column :categories, :piece_id, :integer
+    add_column :categories, :piece_id, :integer #TODO Do I need this line?
   end
 end
