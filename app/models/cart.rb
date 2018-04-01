4c1039dbde8402ebcca9db2d505f6f54c347d540
bretburau <github email address>
2018-04-01 06:19:56 -0500

    Cart line items incrementing instead of duping

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,12 +6,14 @@ class Cart < ApplicationRecord
   def add_item(new_item_id) ##TODO add quantity to orderiing?
     item_in_cart = self.line_items.find_by(item_id: new_item_id)
     if item_in_cart
-      self.item_in_cart.quantity += 1
-      item_in_cart.save
       binding.pry
+      item_in_cart.quantity += 1
+      item_in_cart.save
       self.save
     else
-      self.line_items.build(item_id: new_item_id, quantity: 1)
+      item = self.line_items.build(item_id: new_item_id, quantity: 1)
+      item.save
+      self.save
     end
   end
 
