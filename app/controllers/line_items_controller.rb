f7a77a200fdcf12179b7601b95e400eff8317255
bretburau <github email address>
2018-04-02 05:35:45 -0500

    built create LineItems method, gotaa debug

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,17 @@
 class LineItemsController < ApplicationController
+  def create
+    @piece = Piece.find(params[:item_id])
+    if current_user.current_cart = nil
+      @cart = current_user.carts.build
+      current_user.current_cart = @cart
+    else
+      @cart = current_user.current_cart
+    end
+    binding.pry
+
+    @cart.add_item(@piece)
+    @cart.save
+    current_user.save
+    redirect_to cart_path(@cart)
+  end
 end
